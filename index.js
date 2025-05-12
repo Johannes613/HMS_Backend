db245894cadf14028febaf97acc44ce514c09c7a
Indalu <endalukalbesa511@gmail.com>
2025-05-13 01:51:39 +0400

    project set up

--- index.js
+++ index.js
@@ -1,19 +1,1 @@
-const express = require('express');
-require('dotenv').config();
-const app = express();
-const PORT = 3000;
-const cors=require('cors');
 
-app.use(cors());// Enable CORS for all routes
-
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('Hospital Management System Running');
-});
-
-
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
